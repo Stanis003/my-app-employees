4dd94bdb83d76d5ed005e10423d9ea4037791e0d
”Mandabura <spirit9969@gmail.com>
2021-12-02 22:43:48 +0200

    creating ctructure of app

--- src/index.js
+++ src/index.js
@@ -2,11 +2,7 @@ import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {Header} from './App'
 
-
-
-//const elem = React.createElement('h2', {className:"greetings"},'Hello World' );
 
 ReactDOM.render(
   <StrictMode>
