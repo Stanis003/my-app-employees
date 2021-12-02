946c63e7ae3b1dbe5becf7235b243b08fc5b1dec
”Mandabura <spirit9969@gmail.com>
2021-12-02 23:36:19 +0200

    add app-info component

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,12 @@
+
+import './app-info.css';
+const AppInfo = () => {
+    return(
+        <div className="app-info">
+            <h1>Accounting for employees in the company SoftServe</h1>
+            <h2>Total employees</h2>
+            <h2>The prize will be awarded to:</h2>
+        </div>
+    )
+}
+export default AppInfo;
