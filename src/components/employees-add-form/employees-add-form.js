2cd2d928bbfe5c3928f768daa08cb7f94e501f52
”Mandabura <spirit9969@gmail.com>
2021-12-03 22:51:02 +0200

    create stet in class EmployeeAddForm(Component), managed elements(components), functionValueChange on two states

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -1,21 +1,43 @@
-
+import { Component } from 'react';
 import './employees-add-form.css'
-const EmployeeAddForm = () => {
-    return (
-        <div className="app-add-form">
+class EmployeeAddForm extends Component  {
+    constructor(props){
+        super(props)
+        this.state = {
+            name: '',
+            salary: ''
+        }
+    }
+    onValueChange = (e) => {
+        this.setState({
+           [e.target.name]: e.target.value
+        })
+    }
+    render (){
+
+        const {name, salary} = this.state;
+        return (
+            <div className="app-add-form">
             <h3>Add new employee</h3>
             <form 
                 className="add-form d-flex">
                 <input type="text"
                     className="form-control new-post-label" 
-                    placeholder="Whats her name"/>
+                    placeholder="Whats her name" 
+                    name="name"
+                    value={name}
+                    onChange={this.onValueChange}/>
                 <input type="number"
                     className="form-control new-post-label"
-                    placeholder="Wage in $?" />
+                    placeholder="Wage in $?" 
+                    name="salary" 
+                    value={salary}
+                    onChange={this.onValueChange}/>
                 <button type="submit"
                     className="btn btn-outline-light">Add</button>
             </form>
         </div>
-    )
+        )
+    }
 }
 export default EmployeeAddForm;
