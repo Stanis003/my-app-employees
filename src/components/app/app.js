f66a2a3d93a53a9feaeb9e003305f59073b488c0
”Mandabura <spirit9969@gmail.com>
2021-12-04 16:09:19 +0200

    add functionality - delete employees(employees-list-item) in App

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -5,26 +7,45 @@ import EmployeesList from '../employees-list/employees-list';
 import EmployeeAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
-function App() {
-    const data =[
-        {name:'John C.', salary: 800, increase:true, id: 1},
-        {name:'Max W.', salary: 3000, increase:false, id: 2},
-        {name:'Lex C.', salary: 15000, increase:false, id: 3},
-    ];
 
+class App extends Component {
+    constructor(props){
+        super(props)
+            this.state={
+                data: [
+                    {name:'John C.', salary: 800, increase:true, id: 1},
+                    {name:'Max W.', salary: 3000, increase:false, id: 2},
+                    {name:'Lex C.', salary: 15000, increase:false, id: 3}
+                ]
+            }
+    }
 
-    return(
-       <div className="app"> 
-            <AppInfo/>
+    deleteItem = (id) =>{
+        this.setState(({data})=>{
 
-            <div className="searh-panel">
-            <SearchPanel/>
-            <AppFilter/>
-            </div>  
-            <EmployeesList data={data}/>
-            <EmployeeAddForm/>
-       </div>
+            return {
+                data: data.filter(item=> item.id !== id)
+            }
+
+        })
+    }
+    render(){
+        return(
+            <div className="app"> 
+                 <AppInfo/>
      
-    );
+                 <div className="searh-panel">
+                 <SearchPanel/>
+                 <AppFilter/>
+                 </div>  
+                 <EmployeesList 
+                 data={this.state.data}
+                 onDelete={this.deleteItem}/>
+                 <EmployeeAddForm/>
+            </div>
+          
+         )
+    }
+   
 }
 export default App;
